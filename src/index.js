3ea2d9e31ae1dd46448d09db4d187c94fd02dd3c
Reina <reinayamiletg@gmail.com>
2023-06-03 19:20:14 -0600

    Creacion de carpeta public(index.html,main.css,main.js)

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log('Hola mumdo')
+import express  from "express";
+import { Server as webSocketServer } from "socket.io";
+import http from 'http'
+const app = express()
+const  httpServer = http.createServer(app)
+const io = new webSocketServer(httpServer)
+
+app.use(express.static(__dirname + '/public'))
+
+io.on('connection', () =>{
+    console.log('nueva conexion')
+})
+app.listen(3000)
+console.log('Server on port', 3000)
