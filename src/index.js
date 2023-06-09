ffa46dbfd139c382d01271129c6647afd997d848
Reina <reinayamiletg@gmail.com>
2023-06-08 20:48:24 -0600

    Creacion de formulario

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
 import express  from "express";
 import { Server as webSocketServer } from "socket.io";
 import http from 'http'
+import {v4 as uuid} from 'uuid'
+
+const notes = []
+
 const app = express()
 const  httpServer = http.createServer(app)
 const io = new webSocketServer(httpServer)
 
 app.use(express.static(__dirname + '/public'))
 
-io.on('connection', () =>{
-    console.log('nueva conexion')
+io.on('connection', (socket) =>{
+    console.log('nueva conexion:', socket.id)
+
+    socket.on('client:newnote', newNote =>{
+        const note = {...newNote, id:uuid()}
+        notes.push(note)
+        socket.emit('server:newnote', note)
+    })
+    
 })
 app.listen(3000)
 console.log('Server on port', 3000)
--- src/public/main.js
+++ src/public/main.js
@@ -1,1 +1,15 @@
-console.log('Hola mundo')
+const socket = io()
+
+const noteForm = document.querySelector('#noteForm')
+const title = document.querySelector('#title')
+const description = document.querySelector('#description')
+
+noteForm.addEventListener('submit', e =>{
+    e.preventDefault()
+
+    socket.emit('client:newnote', {
+        title:title.value, 
+        description:description.value 
+    })
+   
+})
